c6b043ac27bc6ce5c2b99566e02d50d8124d152f
Kostya-lab <R156693-Wa@yandex.ru>
2024-09-03 18:19:19 +0300

    add Solution from javaSyntax task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "word";
+        String phrase = "face";
+        String line = "32";
+        String text = "bla bla bla";
+
+    }
+}
