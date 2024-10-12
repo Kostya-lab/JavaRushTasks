bf678c8ea153b0e0ed51dc623ae830a5c16d0976
Kostya-lab <R156693-Wa@yandex.ru>
2024-10-12 21:51:38 +0300

    add Solution from javaSyntax task0301

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task03.task0301;
+
+import java.util.Scanner;
+
+/* 
+Тепло или холодно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String cold = "на улице холодно";
+        String warm = "на улице тепло";
+        Scanner scanner = new Scanner(System.in);
+        int temperature;
+        if (scanner.hasNextInt()) {
+            temperature = scanner.nextInt();
+            if (temperature < 0) {
+                System.out.println(cold);
+            } else {
+                System.out.println(warm);
+            }
+        } else {
+            System.out.println("на улице тепло");
+        }
+        scanner.close();
+            }
+        }
